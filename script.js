900efdc9509788dd66291229f951b87b829017cf
freeflowme <116676605+freeflowme@users.noreply.github.com>
2022-11-30 15:54:52 -0600

    Add function playOneRound()

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
-    const choice = choices[Math.floor(Math.random() * choices.length)];
-    return "The computer chose:" + choice
+    let choice = choices[Math.floor(Math.random() * choices.length)];
+    return choice
 }
 
 /*
 console.log(getComputerChoice())
 */
 
+function playOneRound() {
+    let playerSelection = prompt("Enter one: Rock, Paper, or Scissors").toLowerCase();
+    let computerSelection = getComputerChoice(); 
+    if(playerSelection === computerSelection) {
+        return "Tie!"
+    }
+    if(playerSelection === "Rock") {
+        if(computerSelection === "Scissors") {
+            return "Congratulations, you win!";
+        } else {
+            return "Sorry, you lose.  Better luck next time!";
+        }
+    }
+    if(playerSelection === "Paper") {
+        if(computerSelection === "Rock") {
+            return "Congratulations, you win!";
+        } else {
+            return "Sorry, you lose.  Better luck next time!";
+        }
+    }
+    if(playerSelection === "Scissors") {
+        if(computerSelection === "Paper") {
+            return "Congratulations, you win!";
+        } else {
+            return "Sorry, you lose.  Better luck next time!";
+        }
+    }
+}
