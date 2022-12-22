6466390b1685267711e8cabdfd57813fc41c5804
freeflowme <116676605+freeflowme@users.noreply.github.com>
2022-12-22 14:11:53 -0600

    Add intitial playerScore / computerScore logic

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let playerSelection;
 let computerSelection;
-
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
@@ -16,19 +17,14 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = getComputerChoice().toLowerCase();
 
-    const selectionDiv = document.querySelector("#selectionDiv");
-    const resultsDiv = document.querySelector("#resultsDiv");
-
+    const selectionDiv = document.querySelector("#selectionDiv");  
     const youChose = document.createElement("p");
         youChose.classList.add("youChose");
         youChose.textContent = "You chose " + playerSelection;
-    
         selectionDiv.appendChild(youChose);
-    
     const compyChose = document.createElement("p");
         compyChose.classList.add("compyChose");
         compyChose.textContent = "The computer chose " + computerSelection; 
-    
         selectionDiv.appendChild(compyChose);
 
     /*    
@@ -36,78 +32,62 @@ function playRound(playerSelection, computerSelection) {
     console.log(computerSelection)
     */
 
-    if(playerSelection === computerSelection) {
+    const resultsDiv = document.querySelector("#resultsDiv");
 
-        const isTie = docoument.createElement("p");
-            isTie.classList.add("isTie");
-            isTie.textContent = "It's a tie!";
-        
-            resultsDiv.appendChild(isTie);
-        
+    if(playerSelection === computerSelection) { 
+        const isTie = document.createElement("p");
+        isTie.classList.add("isTie");
+        isTie.textContent = "It's a tie!";
+        resultsDiv.appendChild(isTie);    
         /*
         alert("Tie!");
         */
-
         return "Tie!";
-
     } else if (
         (playerSelection == "rock" && computerSelection == "paper") ||
         (playerSelection == "paper" && computerSelection == "scissors") ||
         (playerSelection == "scissors" && computerSelection == "rock")
-    )   {
-
+    )   { 
             const isLoss = document.createElement("p");
-                isLoss.classList.add("isLoss");
-                isLoss.textContent = "Sorry... you lose";
-
-                resultsDiv.appendChild(isLoss);
-            
+            isLoss.classList.add("isLoss");
+            isLoss.textContent = "Sorry... you lose";
+            resultsDiv.appendChild(isLoss);
             /*    
             alert("Loss!");
             */
-
             return "Loss!";
-
     } else if (
         (playerSelection == "rock" && computerSelection == "scissors") ||
         (playerSelection == "paper" && computerSelection == "rock") ||
         (playerSelection == "scissors" && computerSelection == "paper")
     )   {
-
             const isWin = document.createElement("p");
-                isWin.classList.add("isWin");
-                isWin.textContent = "Congratulations... you win!";
-
-                resultsDiv.appendChild(isWin);
-            
+            isWin.classList.add("isWin");
+            isWin.textContent = "Congratulations... you win!";
+            resultsDiv.appendChild(isWin);
             /*
             alert("Win!");
             */
-
             return "Win!";
     }
 }
 
-/*
+
 function game() {
     for(let i = 1; i < 5; i++) {
         playRound();
     }
 }
 
-game ()
-*/
 
 const rockbBtn = document.querySelector("#rockBtn");
 rockBtn.addEventListener('click', () => {
     playRound("Rock");
 }) 
-
 const paperBtn = document.querySelector("#paperBtn");
 paperBtn.addEventListener('click', () => {
     playRound("Paper");
 })
-
 const scissorsBtn = document.querySelector("#scissorsBtn");
 scissorsBtn.addEventListener('click', () => {
     playRound("Scissors");
